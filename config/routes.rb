028c5fb8b409a517ce5319e06985c74331ec0f99
FrankieMFerreira <FrankieMFerreira@gmail.com>
2017-01-05 13:10:05 -0500

    Don't share your passwords even with your significant others

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get '/new', to: 'users#new', as: 'signup'
+  post '/new', to: 'users#create'
+  get '/', to: 'users#home', as: 'home'
+  get '/login', to: 'users#login', as: 'login'
+  post '/login', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
